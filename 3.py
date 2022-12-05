5a0c45afdf0072989af58635e069986fb87e1f54
GoldenEagle74 <mshchannikov@yandex.ru>
2022-12-05 18:03:23 +0300

    Добавил комментарии

--- 3.py
+++ 3.py
@@ -1,5 +1,7 @@
 # Сложность Q(n)
 
+# Возвращает список с помощью генератора списков в котором проверяется наличие элемента stones в jewels
+
 
 def numJewelsInStones(jewels,stones): return len([x for x in stones if x in jewels])
 
