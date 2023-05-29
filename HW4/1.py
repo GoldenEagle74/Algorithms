6fde610a410fa62ff52c4635737396af0429632a
GoldenEagle74 <mshchannikov@yandex.ru>
2023-05-29 22:05:10 +0300

    Добавил первое задание из HW4

--- HW4/1.py
+++ HW4/1.py
@@ -25,7 +25,3 @@ def twoCitySchedCost(costs) -> int:
     # Возвращаем общую минимальную стоимость полетов
     return cost_first_city + cost_second_city
 
-
-costs = [[259,770],[448,54],[926,667],[184,139],[840,118],[577,469]]
-
-print(twoCitySchedCost(costs))
